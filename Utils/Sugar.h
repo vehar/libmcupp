8505584028190fa7234ebee9ac21372f69596022
emdc <i@emdc.ru>
2016-08-12 11:25:39 +0300

    [Add] Добавлены определения, которые позволят использовать некоторые ключевые слова из С++11 в стандарте С++03

---
+++ Utils/Sugar.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+#include <limits.h>
+
+#ifdef __DEBUG__
+#include "Debug.h"
+#endif
+
+#ifndef nullptr
+#define nullptr NULL
+#endif
+
+#ifndef override
+#define override
+#endif
+
+#ifndef final
+#define final
+#endif
