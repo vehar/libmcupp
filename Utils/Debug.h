d438b7e0ee849ea783dd57b883cb5ccef8e1170d
emdc <i@emdc.ru>
2016-08-12 12:32:22 +0300

    [Add] Добавлены макросы для исключений в Debug

--- Utils/Debug.h
+++ Utils/Debug.h
@@ -9,10 +9,18 @@
 
 #define DEBUG_PRINT(...)								printf(__VA_ARGS__)
 #define DEBUG_CHECK(condition, errfcn, ...)				if(condition) { DEBUG_PRINT(__VA_ARGS__); ##errfcn; }
+#define DEBUG_TRY_BEGIN()								try {
+#define DEBUG_TRY_END()									}
+#define DEBUG_CATCH(exception, fcn)						catch(exception) { ##fcn; }
+#define DEBUG_CATCH(fcn)								catch(...) { ##fcn; }
 
 #else
 
 #define DEBUG_PRINT(...)								
 #define DEBUG_CHECK(condition, errfcn, ...)	
+#define DEBUG_TRY_BEGIN()
+#define DEBUG_TRY_END()
+#define DEBUG_CATCH(exception, fcn)
+#define DEBUG_CATCH(fcn)
 
 #endif
